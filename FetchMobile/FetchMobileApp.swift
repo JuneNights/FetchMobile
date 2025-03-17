66104ae70cc850633629cb717dfe090894c57055
Lates <juniorsj@live.com>
2025-03-17 15:44:42 -0400

    Initial Commit

---
+++ FetchMobile/FetchMobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchMobileApp.swift
+//  FetchMobile
+//
+//  Created by Junior Figuereo on 3/17/25.
+//
+
+import SwiftUI
+
+@main
+struct FetchMobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
